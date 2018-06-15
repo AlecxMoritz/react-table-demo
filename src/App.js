fdc93ed4b60fa5054a6965233b68cf3b85f83581
Alecx Moritz <hanelore@live.com>
2018-06-15 15:41:48 -0400

    initial commit of table demo

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactTable from 'react-table';
+import "react-table/react-table.css";
+
+class App extends Component {
+
+ 
+
+
+  render() {
+
+    const friends = [
+      { 
+        name: 'Nikaya Hale',
+        type: 'Backend Developer' },
+      {
+        name: 'Brittany Hutter',
+        type: 'Fullstack Developer'
+      },
+      {
+        name: 'Autumn Henderson',
+        type: 'Backend Developer'
+      },
+      {
+        name: 'Alecx',
+        type: 'Fullstack Developer'
+      },
+      {
+        name: "Trace Mc'Graw",
+        type: 'Wordpress Developer'
+      },
+      {
+        name: 'Zack Maynard',
+        type: 'Frontend Developer'
+      },
+      {
+        name: 'Tracy Desserich',
+        type: 'Solution Architecht'
+      }
+    ]
+
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Type',
+        accessor: 'type'
+      }
+    ]
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">React Friends List</h1>
+        </header>
+       
+        <ReactTable
+        data={friends}
+        columns={columns}
+        defaultPageSize = {3}
+        pageSizeOptions = {[3, 6]}
+         />
+
+      </div>
+    );
+  }
+}
+
+export default App;
